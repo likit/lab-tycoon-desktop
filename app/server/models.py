7c0fd0971a58ca9a1c46a62d5a992c64a90177b7
Likit Preeyanon <likit.pre@mahidol.edu>
2022-08-04 02:15:12 +0700

    added uniqueness to the email and username fields
    added the lastname column

--- app/server/models.py
+++ app/server/models.py
@@ -6,11 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), auto_increment=True, primary_key=True)
-    username = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    email = db.Column(db.String(255), nullable=True, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
     firstname = db.Column('firstname', db.String(255), nullable=False)
-    lastname = db.Column('firstname', db.String(255), nullable=False)
+    lastname = db.Column('lastname', db.String(255), nullable=False)
     license_id = db.Column('license_id', db.String(255), nullable=True)
 
     @property
