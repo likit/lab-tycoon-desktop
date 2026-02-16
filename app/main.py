64c9e7cef5fc4d98a077899ac23336ca5c21d010
Likit Preeyanon <likit.pre@mahidol.ac.th>
2026-02-16 23:34:18 +0700

    removed unused packages
    changed models to be compatible with SQLAlchemy 2.0 ORM
    
    Refactored code that initializes the database

--- app/main.py
+++ app/main.py
@@ -1,54 +1,19 @@
 import os.path
-import random
+import platform
+import FreeSimpleGUI as sg
+from server.models import initialize_db
 
 BASE_URL = os.getcwd()
-
-import threading
-from faker import Faker
-from client.windows import *
-from server.main import app
-from server.models import *
-
-import platform
 
 if any(platform.win32_ver()):
     from ctypes import windll
 
     windll.shcore.SetProcessDpiAwareness(1)
 
-fake = Faker()
-
 base_url = os.path.dirname(os.path.abspath(__file__))
 
-with app.app_context():
-    if not os.path.exists(os.path.join(base_url, 'instance', 'app.db')):
-        db.create_all()
-        print('Populating a default admin account...')
-        user = User(firstname='Jane', lastname='Doe', username='jane', email='jane@labtycoon.com')
-        user.password = '1234'
-        for role in ['admin', 'approver', 'reporter']:
-            r = UserRole(role_need=role)
-            db.session.add(r)
-            user.roles.append(r)
-        db.session.add(user)
-
-        print('Populating customers database...')
-        for i in range(100):
-            hn = fake.random_number(digits=10)
-            gender = random.choice(['male', 'female'])
-            firstname = fake.first_name()
-            lastname = fake.last_name()
-            address = fake.address()
-            dob = fake.date_of_birth()
-            db.session.add(Customer(
-                hn=hn,
-                gender=gender,
-                firstname=firstname,
-                lastname=lastname,
-                address=address,
-                dob=dob,
-            ))
-        db.session.commit()
+if not os.path.exists(os.path.join(base_url, 'instance', 'app.db')):
+    initialize_db()
 
 sg.theme('SystemDefault')
 sg.set_options(font=('Helvetica', 12))
@@ -79,85 +44,80 @@ layout = [
 window = sg.Window('Lab Tycoon Desktop!',
                    layout=layout,
                    element_justification='center').finalize()
-flask_job = threading.Thread(target=lambda: app.run(use_reloader=False, debug=True))
-flask_job.start()
-access_token = ''
 
 while True:
     event, values = window.read()
     if event == sg.WINDOW_CLOSED or event == 'Exit':
-        print('Trying to terminate Flask app')
-        requests.get('http://127.0.0.1:5000/kill')
-        flask_job.join()
         break
-    elif event == 'Register':
-        if not access_token:
-            sg.popup_error('Please sing in to access this section.')
-        else:
-            create_register_window()
-    elif event == '-SIGNIN-':
-        access_token = create_signin_window()
-        if access_token:
-            window.find_element('-SIGNOUT-').update(visible=True)
-            window.find_element('-EDIT-PROFILE-').update(visible=True)
-            window.find_element('-SIGNIN-').update(visible=False)
-    elif event == '-SIGNOUT-':
-        if sg.popup_yes_no('You sure want to sign out?') == 'Yes':
-            headers = {'Authorization': f'Bearer {access_token}'}
-            resp = requests.delete('http://127.0.0.1:5000/auth/sign-out', headers=headers)
-            if resp.status_code == 200:
-                sg.popup_auto_close('You have logged out.')
-                # TODO: store current_user in the database so that it is accessible outside Flask
-                window.find_element('-SIGNOUT-').update(visible=False)
-                window.find_element('-SIGNIN-').update(visible=True)
-                window.find_element('-EDIT-PROFILE-').update(visible=True)
-            else:
-                sg.popup_error(resp.json().get('message'))
-    elif event == '-EDIT-PROFILE-':
-        if access_token:
-            create_profile_window(access_token)
-        else:
-            sg.popup_error('Please sign in to access this section.', title='Access Denied')
-    elif event == 'Program':
-        sg.popup_ok('This program is developed by Asst. Prof.Likit Preeyanon. '
-                    'Please contact likit.pre@mahidol.edu for more information.'
-                    , title='About')
-    elif event == 'SQL Editor':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.', title='Access Denied')
-        else:
-            create_sql_window()
-    elif event == '-ANALYZE-':
-        create_analysis_window(access_token)
-    elif event == '-order-list-':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.', title='Access Denied')
-        else:
-            create_order_list_window(access_token)
-    elif event == '-LOGGING-':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.', title='Access Denied')
-        else:
-            create_logging_window(access_token)
-    elif event == 'Manage':
-        create_user_list_window(access_token)
-    elif event == 'BioSource':
-        create_biosource_window(access_token)
-    elif event == 'Add TMLT test':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.')
-        else:
-            create_tmlt_test_window(access_token)
-    elif event == 'List':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.')
-        else:
-            create_test_list_window(access_token)
-    elif event == '-PATIENT-':
-        if not access_token:
-            sg.popup_error('Please sign in to access this section.')
-        else:
-            create_customer_list_window(access_token)
+    # elif event == 'Register':
+    #     if not access_token:
+    #         sg.popup_error('Please sing in to access this section.')
+    #     else:
+    #         # create_register_window()
+    #         print('foo')
+    # elif event == '-SIGNIN-':
+    #     access_token = None
+    #     if access_token:
+    #         window.find_element('-SIGNOUT-').update(visible=True)
+    #         window.find_element('-EDIT-PROFILE-').update(visible=True)
+    #         window.find_element('-SIGNIN-').update(visible=False)
+    # elif event == '-SIGNOUT-':
+    #     if sg.popup_yes_no('You sure want to sign out?') == 'Yes':
+    #         headers = {'Authorization': f'Bearer {access_token}'}
+    #         # resp = requests.delete('http://127.0.0.1:5000/auth/sign-out', headers=headers)
+    #         if resp.status_code == 200:
+    #             sg.popup_auto_close('You have logged out.')
+    #             # TODO: store current_user in the database so that it is accessible outside Flask
+    #             window.find_element('-SIGNOUT-').update(visible=False)
+    #             window.find_element('-SIGNIN-').update(visible=True)
+    #             window.find_element('-EDIT-PROFILE-').update(visible=True)
+    #         else:
+    #             sg.popup_error(resp.json().get('message'))
+    # elif event == '-EDIT-PROFILE-':
+    #     if access_token:
+    #         create_profile_window(access_token)
+    #     else:
+    #         sg.popup_error('Please sign in to access this section.', title='Access Denied')
+    # elif event == 'Program':
+    #     sg.popup_ok('This program is developed by Asst. Prof.Likit Preeyanon. '
+    #                 'Please contact likit.pre@mahidol.edu for more information.'
+    #                 , title='About')
+    # elif event == 'SQL Editor':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.', title='Access Denied')
+    #     else:
+    #         create_sql_window()
+    # elif event == '-ANALYZE-':
+    #     create_analysis_window(access_token)
+    # elif event == '-order-list-':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.', title='Access Denied')
+    #     else:
+    #         create_order_list_window(access_token)
+    # elif event == '-LOGGING-':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.', title='Access Denied')
+    #     else:
+    #         create_logging_window(access_token)
+    # elif event == 'Manage':
+    #     create_user_list_window(access_token)
+    # elif event == 'BioSource':
+    #     create_biosource_window(access_token)
+    # elif event == 'Add TMLT test':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.')
+    #     else:
+    #         create_tmlt_test_window(access_token)
+    # elif event == 'List':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.')
+    #     else:
+    #         create_test_list_window(access_token)
+    # elif event == '-PATIENT-':
+    #     if not access_token:
+    #         sg.popup_error('Please sign in to access this section.')
+    #     else:
+    #         create_customer_list_window(access_token)
 
 
 window.close()
--- app/server/models.py
+++ app/server/models.py
@@ -1,37 +1,38 @@
-from sqlalchemy import func
-import sqlalchemy
+import os
+import random
+
+import bcrypt
+from faker import Faker
+from typing import List
+
 from sqlalchemy_continuum import make_versioned
-from sqlalchemy_continuum.plugins import FlaskPlugin
-from flask_jwt_extended import get_current_user
+from sqlalchemy import create_engine
+from sqlalchemy import (
+    ForeignKey, String, Integer,
+    Table, Column, Boolean,
+    Text, Numeric, Date,
+    DateTime
+)
+from sqlalchemy.orm import DeclarativeBase, configure_mappers, Session, dynamic_loader
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from server.extensions import db
-from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, date
 
-make_versioned(plugins=[FlaskPlugin()])
+make_versioned()
 
-
-class TokenBlocklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jti = db.Column(db.String(36), nullable=False, index=True)
-    type = db.Column(db.String(16), nullable=False)
-    user_id = db.Column(
-        db.ForeignKey('users.id'),
-        default=lambda: get_current_user().id,
-        nullable=False,
-    )
-    created_at = db.Column(
-        db.DateTime,
-        server_default=func.now(),
-        nullable=False,
-    )
+engine = create_engine('sqlite:///:memory:')
 
 
-class UserRole(db.Model):
+class Base(DeclarativeBase):
+    pass
+
+
+class UserRole(Base):
     __tablename__ = 'roles'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    role_need = db.Column('role_need', db.String(), nullable=True)
-    action_need = db.Column('action_need', db.String())
-    resource_id = db.Column('resource_id', db.Integer())
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+    role_need: Mapped[str] = mapped_column('role_need', String(), nullable=True)
+    action_need: Mapped[str] = mapped_column('action_need', String(), nullable=True)
+    resource_id: Mapped[int] = mapped_column('resource_id', Integer(), nullable=True)
 
     def to_tuple(self):
         return self.role_need, self.action_need, self.resource_id
@@ -40,23 +41,23 @@ class UserRole(db.Model):
         return u'Role {}: can {} -> resource ID {}'.format(self.role_need, self.action_need, self.resource_id)
 
 
-user_roles = db.Table('user_roles',
-                      db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
-                      db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
+user_roles = Table('user_roles', Base.metadata,
+                   Column('user_id', Integer(), ForeignKey('users.id')),
+                   Column('role_id', Integer(), ForeignKey('roles.id')))
 
 
-class User(db.Model):
+class User(Base):
     __tablename__ = 'users'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    username = db.Column(db.String(255), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=True, unique=True)
-    hashed_password = db.Column(db.String(255), nullable=False)
-    firstname = db.Column('firstname', db.String(255), nullable=False)
-    lastname = db.Column('lastname', db.String(255), nullable=False)
-    license_id = db.Column('license_id', db.String(255), nullable=True)
-    position = db.Column('position', db.String(255), nullable=True)
-    roles = db.relationship(UserRole, secondary=user_roles, backref=db.backref('users', lazy='dynamic'))
-    active = db.Column('active', db.Boolean(), default=True)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    username: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(String(255), nullable=True, unique=True)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    firstname: Mapped[str] = mapped_column('firstname', String(255), nullable=False)
+    lastname: Mapped[str] = mapped_column('lastname', String(255), nullable=False)
+    license_id: Mapped[int] = mapped_column('license_id', String(255), nullable=True)
+    position: Mapped[int] = mapped_column('position', String(255), nullable=True)
+    roles: Mapped[List["UserRole"]] = relationship(secondary=user_roles)
+    active: Mapped[bool] = mapped_column('active', Boolean(), default=True)
 
     @classmethod
     def get_user_by_username(cls, username):
@@ -68,10 +69,10 @@ class User(db.Model):
 
     @password.setter
     def password(self, new_password):
-        self.hashed_password = generate_password_hash(new_password)
+        self.hashed_password = bcrypt.hashpw(new_password.encode('UTF-8'), bcrypt.gensalt())
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        return bcrypt.checkpw(password, self.hashed_password)
 
     def to_dict(self):
         return {
@@ -93,53 +94,57 @@ class User(db.Model):
         return role_need in [r.role_need for r in self.roles]
 
 
-class BioSource(db.Model):
+class BioSource(Base):
     __tablename__ = 'biosources'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    source = db.Column('source', db.String(255), nullable=False)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    source: Mapped[str] = mapped_column('source', String(255), nullable=False)
+    specimens: Mapped[List["Specimens"]] = relationship(back_populates="source", cascade='all, delete-orphan')
 
 
-class Specimens(db.Model):
+class Specimens(Base):
     __tablename__ = 'specimens'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    label = db.Column('label', db.String(255), nullable=False)
-    source_id = db.Column('source_id', db.ForeignKey('biosources.id'))
-    source = db.relationship(BioSource, backref=db.backref('specimens', lazy='dynamic', cascade='all, delete-orphan'))
-    desc = db.Column('desc', db.Text(), nullable=True)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    label: Mapped[str] = mapped_column('label', String(255), nullable=False)
+    source_id: Mapped[int] = mapped_column('source_id', ForeignKey('biosources.id'))
+    source: Mapped["BioSource"] = relationship(back_populates="specimens")
+    desc: Mapped[str] = mapped_column('desc', Text(), nullable=True)
+    tests: Mapped[List["Test"]] = relationship(back_populates="specimens")
 
 
-class TestMethod(db.Model):
+class TestMethod(Base):
     __tablename__ = 'test_methods'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    method = db.Column('method', db.String(), nullable=False)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    method: Mapped[str] = mapped_column('method', String(), nullable=False)
+    tests: Mapped[List["Test"]] = relationship(back_populates="method")
 
 
-class Test(db.Model):
+class Test(Base):
     __tablename__ = 'tests'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    code = db.Column('code', db.String(), nullable=False, unique=True, index=True)
-    tmlt_code = db.Column('tmlt_code', db.String(), unique=True, index=True)
-    tmlt_name = db.Column('tmlt_name', db.String())
-    loinc_no = db.Column('loinc_no', db.String(), unique=True)
-    component = db.Column('component', db.String())
-    label = db.Column('label', db.String(), nullable=False)
-    scale = db.Column('scale', db.String(), nullable=False)
-    specimens_id = db.Column(db.ForeignKey('specimens.id'))
-    method_id = db.Column(db.ForeignKey('test_methods.id'))
-    price = db.Column('price', db.Numeric(), default=0.0, nullable=False)
-    desc = db.Column('desc', db.Text())
-    unit = db.Column('unit', db.String(), nullable=False)
-    order_type = db.Column('order_type', db.String())
-    cgd_code = db.Column('cgd_code', db.String(), unique=True)
-    cgd_name = db.Column('cgd_name', db.String())
-    cgd_price = db.Column('cgd_price', db.Numeric())
-    panel = db.Column('panel', db.String())
-    ref_min = db.Column('ref_min', db.Numeric())
-    ref_max = db.Column('ref_max', db.Numeric())
-    value_choices = db.Column('value_choices', db.String())
-    method = db.relationship(TestMethod, backref=db.backref('tests'))
-    specimens = db.relationship(Specimens, backref=db.backref('tests'))
-    active = db.Column('active', db.Boolean(), default=True)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    code: Mapped[int] = mapped_column('code', String(), nullable=False, unique=True, index=True)
+    tmlt_code: Mapped[int] = mapped_column('tmlt_code', String(), unique=True, index=True)
+    tmlt_name: Mapped[int] = mapped_column('tmlt_name', String())
+    loinc_no: Mapped[int] = mapped_column('loinc_no', String(), unique=True)
+    component: Mapped[int] = mapped_column('component', String())
+    label: Mapped[int] = mapped_column('label', String(), nullable=False)
+    scale: Mapped[int] = mapped_column('scale', String(), nullable=False)
+    specimens_id: Mapped[int] = mapped_column(ForeignKey('specimens.id'))
+    method_id: Mapped[int] = mapped_column(ForeignKey('test_methods.id'))
+    price: Mapped[int] = mapped_column('price', Numeric(), default=0.0, nullable=False)
+    desc: Mapped[int] = mapped_column('desc', Text())
+    unit: Mapped[int] = mapped_column('unit', String(), nullable=False)
+    order_type: Mapped[int] = mapped_column('order_type', String())
+    cgd_code: Mapped[int] = mapped_column('cgd_code', String(), unique=True)
+    cgd_name: Mapped[int] = mapped_column('cgd_name', String())
+    cgd_price: Mapped[int] = mapped_column('cgd_price', Numeric())
+    panel: Mapped[int] = mapped_column('panel', String())
+    ref_min: Mapped[int] = mapped_column('ref_min', Numeric())
+    ref_max: Mapped[int] = mapped_column('ref_max', Numeric())
+    value_choices: Mapped[int] = mapped_column('value_choices', String())
+    method: Mapped["TestMethod"] = relationship(back_populates="tests")
+    specimens: Mapped["Specimens"] = relationship(back_populates="tests")
+    active: Mapped[bool] = mapped_column('active', Boolean(), default=True)
+    order_items: Mapped[List["LabOrderItem"]] = relationship(back_populates="test", lazy="dynamic")
 
     def __init__(self, code, tmlt_code, tmlt_name, loinc_no, specimens, method,
                  component, label, scale, price, desc, unit, order_type,
@@ -189,15 +194,16 @@ class Test(db.Model):
         }
 
 
-class Customer(db.Model):
+class Customer(Base):
     __tablename__ = 'customers'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    hn = db.Column('hn', db.String(), unique=True)
-    firstname = db.Column('firstname', db.String())
-    lastname = db.Column('lastname', db.String())
-    gender = db.Column('gender', db.String())
-    dob = db.Column('dob', db.Date())
-    address = db.Column('address', db.Text())
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    hn: Mapped[int] = mapped_column('hn', String(), unique=True)
+    firstname: Mapped[int] = mapped_column('firstname', String())
+    lastname: Mapped[int] = mapped_column('lastname', String())
+    gender: Mapped[int] = mapped_column('gender', String())
+    dob: Mapped[date] = mapped_column('dob', Date())
+    address: Mapped[int] = mapped_column('address', Text())
+    orders: Mapped[List["LabOrder"]] = relationship(back_populates="customer", cascade="all, delete-orphan")
 
     @property
     def fullname(self):
@@ -215,47 +221,49 @@ class Customer(db.Model):
         }
 
 
-class LabOrder(db.Model):
+class LabOrder(Base):
     __tablename__ = 'lab_orders'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    customer_id = db.Column('customer_id', db.ForeignKey('customers.id'))
-    order_datetime = db.Column('order_datetime', db.DateTime())
-    cancelled_at = db.Column('cancelled_at', db.DateTime())
-    canceller_id = db.Column('canceller_id', db.ForeignKey('users.id'))
-    canceller = db.relationship(User, foreign_keys=[canceller_id])
-    received_at = db.Column('received_at', db.DateTime())
-    customer = db.relationship(Customer, backref=db.backref('orders'))
-    released_at = db.Column('released_at', db.DateTime())
-    rejected_at = db.Column('rejected_at', db.DateTime())
-    reason = db.Column('reason', db.String())
-    comment = db.Column('comment', db.Text())
-    rejector_id = db.Column('rejector_id', db.ForeignKey('users.id'))
-    rejector = db.relationship(User, foreign_keys=[rejector_id])
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    customer_id: Mapped[int] = mapped_column('customer_id', ForeignKey('customers.id'))
+    order_datetime: Mapped[datetime] = mapped_column('order_datetime', DateTime())
+    cancelled_at: Mapped[datetime] = mapped_column('cancelled_at', DateTime())
+    canceller_id: Mapped[int] = mapped_column('canceller_id', ForeignKey('users.id'))
+    canceller: Mapped["User"] = relationship(foreign_keys=[canceller_id])
+    received_at: Mapped[datetime] = mapped_column('received_at', DateTime())
+    customer: Mapped["Customer"] = relationship(back_populates="orders")
+    released_at: Mapped[datetime] = mapped_column('released_at', DateTime())
+    rejected_at: Mapped[datetime] = mapped_column('rejected_at', DateTime())
+    reason: Mapped[str] = mapped_column('reason', String())
+    comment: Mapped[str] = mapped_column('comment', Text())
+    rejector_id: Mapped[int] = mapped_column('rejector_id', ForeignKey('users.id'))
+    rejector: Mapped["User"] = relationship(foreign_keys=[rejector_id])
+    order_items: Mapped[List["LabOrderItem"]] = relationship(back_populates="order", cascade="all, delete-orphan")
 
 
-class LabOrderItem(db.Model):
+class LabOrderItem(Base):
     __versioned__ = {}
     __tablename__ = 'lab_order_items'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    order_id = db.Column('order_id', db.ForeignKey('lab_orders.id'))
-    test_id = db.Column('test_id', db.ForeignKey('tests.id'))
-    test = db.relationship(Test, backref=db.backref('order_items', lazy='dynamic', cascade='all, delete-orphan'))
-    order = db.relationship(LabOrder, backref=db.backref('order_items', lazy='dynamic', cascade='all, delete-orphan'))
-    comment = db.Column('comment', db.Text())
-    cancelled_at = db.Column('cancelled_at', db.DateTime())
-    finished_at = db.Column('finished_at', db.DateTime())
-    updated_at = db.Column('updated_at', db.DateTime())
-    reported_at = db.Column('reported_at', db.DateTime())
-    approved_at = db.Column('approved_at', db.DateTime())
-    approver_id = db.Column('approver_id', db.ForeignKey('users.id'))
-    updater_id = db.Column('updater_id', db.ForeignKey('users.id'))
-    reporter_id = db.Column('reporter_id', db.ForeignKey('users.id'))
-    canceller_id = db.Column('canceller_id', db.ForeignKey('users.id'))
-    approver = db.relationship(User, foreign_keys=[approver_id])
-    updater = db.relationship(User, foreign_keys=[updater_id])
-    reporter = db.relationship(User, foreign_keys=[reporter_id])
-    canceller = db.relationship(User, foreign_keys=[canceller_id])
-    _value = db.Column('value', db.String(), nullable=True)
+    id: Mapped[int] = mapped_column(Integer(), autoincrement=True, primary_key=True)
+    order_id: Mapped[int] = mapped_column('order_id', ForeignKey('lab_orders.id'))
+    test_id: Mapped[int] = mapped_column('test_id', ForeignKey('tests.id'))
+    test: Mapped["Test"] = relationship(back_populates='order_items')
+    order: Mapped["LabOrder"] = relationship(back_populates='order_items')
+    comment: Mapped[str] = mapped_column('comment', Text())
+    cancelled_at: Mapped[datetime] = mapped_column('cancelled_at', DateTime())
+    finished_at: Mapped[datetime] = mapped_column('finished_at', DateTime())
+    updated_at: Mapped[datetime] = mapped_column('updated_at', DateTime())
+    reported_at: Mapped[datetime] = mapped_column('reported_at', DateTime())
+    approved_at: Mapped[datetime] = mapped_column('approved_at', DateTime())
+    approver_id: Mapped[int] = mapped_column('approver_id', ForeignKey('users.id'))
+    updater_id: Mapped[int] = mapped_column('updater_id', ForeignKey('users.id'))
+    reporter_id: Mapped[int] = mapped_column('reporter_id', ForeignKey('users.id'))
+    canceller_id: Mapped[int] = mapped_column('canceller_id', ForeignKey('users.id'))
+    approver: Mapped["User"] = relationship(foreign_keys=[approver_id])
+    updater: Mapped["User"] = relationship(foreign_keys=[updater_id])
+    reporter: Mapped["User"] = relationship(foreign_keys=[reporter_id])
+    canceller: Mapped["User"] = relationship(foreign_keys=[canceller_id])
+    _value: Mapped[str] = Column('value', String(), nullable=True)
+    reject_records: Mapped[List["LabRejectRecord"]] = relationship(back_populates="order_item", cascade="all, delete-orphan")
 
     @property
     def value(self):
@@ -292,11 +300,46 @@ class LabOrderItem(db.Model):
         }
 
 
-class LabRejectRecord(db.Model):
+class LabRejectRecord(Base):
     __tablename__ = 'lab_reject_records'
-    id = db.Column(db.Integer(), autoincrement=True, primary_key=True)
-    order_item_id = db.Column('order_item_id', db.ForeignKey('lab_order_items.id'))
-    order_item = db.relationship(LabOrderItem, backref=db.backref('reject_records'))
+    id: Mapped[int] = Column(Integer(), autoincrement=True, primary_key=True)
+    order_item_id: Mapped[int] = Column('order_item_id', ForeignKey('lab_order_items.id'))
+    order_item: Mapped["LabOrderItem"] = relationship(back_populates='reject_records')
 
 
-sqlalchemy.orm.configure_mappers()
+configure_mappers()
+
+def initialize_db():
+    Base.metadata.create_all(engine)
+    print('Populating a default admin account...')
+    user = User(firstname='Jane', lastname='Doe', username='jane', email='jane@labtycoon.com')
+    user.password = '1234'
+    for role in ['admin', 'approver', 'reporter']:
+        r = UserRole(role_need=role)
+        user.roles.append(r)
+
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+
+    fake = Faker()
+    print('Populating customers database...')
+    customers = []
+    for i in range(100):
+        hn = fake.random_number(digits=10)
+        gender = random.choice(['male', 'female'])
+        firstname = fake.first_name()
+        lastname = fake.last_name()
+        address = fake.address()
+        dob = fake.date_of_birth()
+
+        customers.append(Customer(hn=hn,
+                                  gender=gender,
+                                  firstname=firstname,
+                                  lastname=lastname,
+                                  address=address,
+                                  dob=dob)
+                         )
+    with Session(engine) as session:
+        session.add_all(customers)
+        session.commit()
